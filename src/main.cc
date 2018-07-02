3d5aeaf410560afc02b4c6c31cc6eefbc78b24f4
Yi Qiao <atrusqiao@gmail.com>
2018-07-02 12:30:30 -0600

    implemented basic echo function that pass input vcf directly to the output

--- src/main.cc
+++ src/main.cc
@@ -5,6 +5,22 @@
 using namespace YiCppLib::HTSLibpp;
 
 int main(int argc, const char* argv[]) {
-    auto htsFileHandler = htsOpen("-", "r");
+    auto htsInHandle = htsOpen("-", "r");
+    auto htsOutHandle = htsOpen("-", "w");
+
+    auto header = htsHeader<bcfHeader>::read(htsInHandle);
+
+    if(header.get() == nullptr) {
+        std::cerr<<"unable to read header from input stream"<<std::endl;
+        exit(1);
+    }
+
+    bcf_hdr_write(htsOutHandle.get(), header.get());
+
+    for_each(begin(htsInHandle, header), end(htsInHandle, header), [&htsOutHandle, &header](auto &bcfRecord) {
+        bcf_write(htsOutHandle.get(), header.get(), bcfRecord.get());
+    });
+
+
     return 0;
 }
